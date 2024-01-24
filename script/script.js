59c6832279ceac6ba589686716dedec21b61c45c
goonyboy <samalexzol@gmail.com>
2024-01-24 03:26:01 +0300

    Коммит через git bash

--- script/script.js
+++ script/script.js
@@ -1,20 +1,38 @@
 //Открытие изображение при нажатии на него
 
-document.querySelectorAll('.card img').forEach(img => {
+
+try {document.querySelectorAll('.card img').forEach(img => {
   img.onclick = () => {
     document.querySelector('.pop-up').style.display = 'block';
     document.querySelector('.pop-up img').src = img.getAttribute('src')
   }
 });
+} catch (err) {
+  console.log('Нет изображения, поэтому скрипт не может быть выполнен')
+}
+
 
 //Закрытие изображения по нажатию на крестик
-document.querySelector('.pop-up span').onclick = () => {
+try {document.querySelector('.pop-up span').onclick = () => {
   document.querySelector('.pop-up').style.display = 'none';
 };
+} catch (err) {
+  console.log('Нет крестика, поэтому скрипт не может быть выполнен')
+}
 
-// document.addEventListener('keypress', e => {
-//   if (e.keyCode == 27) document.querySelector('.pop-up').style.display = 'none';
-// });
+//Закрытие изображения по нажатию на Esc
+//Можно усовершенствовать, посколько событие срабатывает даже, когда изображение не открыто
+try {
+  document.onkeydown = (e) => {
+      if (e.keyCode == '27')
+      {
+        document.querySelector('.pop-up').style.display = 'none';
+      }
+  };
+
+} catch (err) {
+  console.log('Не работает событие по нажатию Esc')
+}
 
 
 //Бургер меню с помощью добавления класса open header'у
